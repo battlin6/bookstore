56f97b464dba46a7b5d30dd917c77b94bf0ab5f3
battlin6 <55840320+battlin6@users.noreply.github.com>
2020-01-13 00:46:15 +0800

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -2,39 +2,99 @@
 // Created by 52816 on 2020/1/7.
 //
 
-#ifndef BOOKSTORE_BOOK_H
-#define BOOKSTORE_BOOK_H
+#include "Book.h"
 
-#include <iostream>
-#include <cstring>
-#include <bits/stdc++.h>
-using namespace std;
+Book::Book(const char* s1,const char* s2,const char* s3,const char* s4,const int &s5,const double &s6) {
+    Price=s6;
+    Quti=s5;
+    OK=true;
+    strcpy(ISBN,s1);
+    strcpy(Name,s2);
+    strcpy(Author,s3);
+    strcpy(Keyword,s4);
+}
 
-class Book {
-private:
-    bool OK;
-    double Price;
-    int Quti;
-    char ISBN[23],Name[63],Author[63],Keyword[63];
-public:
-    explicit Book(const char* s1="",const char* s2="",const char* s3="",const char* s4="",const int &s5=0,const double &s6=0);
-    void Delete();
-    bool getOK();
-    double getPrice();
-    string getISBN();
-    void ModifyISBN(const char *s);
-    void ModifyName(const char *s);
-    void ModifyAuthor(const char *s);
-    void ModifyKeyword(const char *s);
-    void ModifyPrice(const char *s);
-    void show();
-    bool Buy(const int &s);
-    void AddBook(const char*s);
-    bool Check_ISBN(const char *s);
-    bool Check_Name(const char *s);
-    bool Check_Author(const char *s);
-    bool Check_Keyword(const char *s);
-};
+void Book::Delete() {
+    OK=false;
+}
 
+bool Book::getOK() {
+    return OK;
+}
 
-#endif //BOOKSTORE_BOOK_H
+double Book::getPrice() {
+    return Price;
+}
+
+void Book::show() {
+    printf("%s\t%s\t%s\t%s\t%.2f\t%d本\n",ISBN,Name,Author,Keyword,Price,Quti);
+}
+
+string Book::getISBN() {
+    return ISBN;
+}
+
+bool Book::Buy(const int &s) {
+    if(s<=Quti){
+        Quti-=s;
+        return true;
+    }
+    return false;
+}
+
+void Book::ModifyISBN(const char *s) {
+    strcpy(ISBN,s);
+}
+
+void Book::ModifyPrice(const char *s) {
+    sscanf(s,"%lf",&Price);
+}
+
+void Book::ModifyName(const char *s) {
+    strcpy(Name,s);
+}
+
+void Book::ModifyAuthor(const char *s) {
+    strcpy(Author,s);
+}
+
+void Book::ModifyKeyword(const char *s) {
+    strcpy(Keyword,s);
+}
+
+void Book::AddBook(const char*s) {
+    int add;
+    sscanf(s,"%d",&add);
+    Quti+=add;
+}
+
+bool Book::Check_ISBN(const char* s) {
+    return strcmp(ISBN,s)==0;
+}
+
+bool Book::Check_Name(const char* s) {
+    return strcmp(Name,s)==0;
+}
+
+bool Book::Check_Author(const char *s) {
+    return strcmp(Author,s)==0;
+}
+
+bool Book::Check_Keyword(const char *s) {
+    for(int i=0;i<strlen(Keyword);++i){
+        int pos=i+1;
+        while(pos<strlen(Keyword)&&Keyword[pos]!='|') pos++;
+        if(pos-i==strlen(s)) {
+            bool flag = true;
+            for (int j = 0; j < strlen(s); ++j) {
+                if (Keyword[i + j] != s[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) return true;
+        }
+        i=pos;
+    }
+    return false;
+}
